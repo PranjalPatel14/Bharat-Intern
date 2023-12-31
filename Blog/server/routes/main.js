7464190face3b80b42bd8c30f9c90d67e3a3eeea
Pranjal Patel <patelpranjal139@gmail.com>
2023-12-31 12:59:49 +0530

    Added Pdf in gitignore

--- Blog/server/routes/main.js
+++ Blog/server/routes/main.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('',(req,res)=>{
+router.get('', (req, res) => {
     const locals = {
         title: "Node JS Blog",
         description: "Simple Blog Website using mongo DB, Express JS."
     }
-    res.render('index',{locals});
+
+    try{
+        const data = Post.find();
+        res.render('index', {locals, data });
+    } catch(error){
+        console.log(error);
+    }
 });
 
-router.get('',(req,res)=>{
+
+router.get('/about', (req, res) => {
     res.render('about');
 });
 
+// function insertPostData() {
+//     Post.insertMany([
+//         {
+//             title: "Building a blog",
+//             body: "This is the body text"
+//         },
+//         {
+//             title: "Deployment of Node.js applications",
+//             body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
+//         },
+//         {
+//             title: "Authentication and Authorization in Node.js",
+//             body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
+//         },
+//         {
+//             title: "Understand how to work with MongoDB and Mongoose",
+//             body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
+//         },
+//         {
+//             title: "build real-time, event-driven applications in Node.js",
+//             body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
+//         }
+//     ])
+// }
 
-module.exports =  router;
+module.exports = router;

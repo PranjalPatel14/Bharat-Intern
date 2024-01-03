ab610413807bf652d1cb014685f80ee2cab72634
Pranjal Patel <patelpranjal139@gmail.com>
2024-01-03 23:34:46 +0530

    Updated Features

--- Blog/server/routes/main.js
+++ Blog/server/routes/main.js
@@ -37,19 +37,28 @@ router.get('/post/:id', async (req, res) => {
     }
 });
 
-router.post('/search', (req, res) => {
+router.post('/search', async (req, res) => {
     try{
         const locals = {
             title: "Search",
             description: "Simple Blog Website using mongo DB, Express JS."
         }
-        const data = Post.find();
         let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g,"")
 
-        console.log(searchTerm);
+        const data = await Post.find({
+            $or: [
+                { title: { $regex: new RegExp(searchNoSpecialChar,'i')}},
+                { body: {$regex: new RegExp(searchNoSpecialChar, 'i')}}
+            ]
+        });
 
 
-        res.render(searchTerm);
+
+        res.render("search",{
+            data,
+            locals
+        });
     } catch(error){
         console.log(error); 
     }

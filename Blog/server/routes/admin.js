676f4494bf9341c31deca8845b9892b24a57c1e0
Pranjal Patel <patelpranjal139@gmail.com>
2024-01-09 18:56:38 +0530

    Added Admin Dashboard with add post option

--- Blog/server/routes/admin.js
+++ Blog/server/routes/admin.js
@@ -5,6 +5,29 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const dir = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
+
+
+
+
+// Check Login
+const checkMiddleware = (res,req,next)=>{
+    const token  = res.cookies.token;
+
+    if(!token){
+        res.render('admin/logout');
+    }
+
+    try{
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    }catch(error){
+        res.render('admin/logout');
+    }
+}
+
+
 
 // GET
 // ADMIN - LOGIN PAGE
@@ -36,7 +59,7 @@ function insertMany(req_username, req_password) {
 // POST
 // ADMIN - LOGIN CHECK
 
-router.post('/login', async (req, res) => {
+router.post('/admin', async (req, res) => {
     try{
         const { username , password } = req.body;        
         const user = await User.findOne({username}); 
@@ -44,10 +67,12 @@ router.post('/login', async (req, res) => {
         if (user){
             if(user.password == password){
                 // res.status(200).json({message: 'Login Successful'});
-                res.redirect('/main')
+                const token = jwt.sign({ userId: user._id}, jwtSecret);
+                res.cookie('token',token,{ httpOnly: true});
+                res.redirect('/dashboard');
             } else {
                 res.status(401).json({message: "Incorrect Password"});
-            }w
+            }
         }
         else {
             res.status(400).json({message: "User Not Found"});
@@ -58,29 +83,82 @@ router.post('/login', async (req, res) => {
     }
 });
 
+// GET
+// ADMIN - DASHBOARD CHECK
+
+router.get('/dashboard', checkMiddleware, async (req, res) => {
+    try{
+        locals = {
+            title: 'Dashboard',
+            description : "Simple Blog Website using mongo DB, Express JS."
+        }
+
+        const data = await Post.find();
+        res.render('admin/dashboard',{
+            locals,data,layout:dir
+        });
+    } catch(error){
+        console.log(error);
+    }
+})
+
 // POST
 // ADMIN - Register
 
-router.post('/register', async (req, res) => {
-    try{
-        const { requsername , reqpassword } = req.body;        
-        const user = await User.findOne({requsername}); 
+// router.post('/register', async (req, res) => {
+//     try{
+//         const { requsername , reqpassword } = req.body;        
+//         const user = await User.findOne({requsername}); 
 
-        if (user){
-            if(user.reqpassword == password){
-                res.status(200).json({message: 'Login Already Present'});
-            }// else {
-            //     res.status(401).json({message: "Incorrect Password"});
-            // }
+//         if (user){
+//             if(user.reqpassword == password){
+//                 res.status(200).json({message: 'Login Already Present'});
+//             }// else {
+//             //     res.status(401).json({message: "Incorrect Password"});
+//             // }
+//         }
+//         else {
+//             insertMany(requsername, reqpassword);
+//             res.status(200).json({message: 'Registration Done'});
+//         }
+//     } catch(error){
+//         console.error(error);
+//         res.status(500).json({message : 'Internal Server Error'});
+//     }
+// });
+
+
+// GET
+// ADD-POST - NEW BLOG ROUTE
+
+router.get('/add-post', checkMiddleware, async(req,res)=>{
+    try{
+        locals = {
+            title: 'Add Post',
+            description : "Simple Blog Website using mongo DB, Express JS."
         }
-        else {
-            insertMany(requsername, reqpassword);
-            res.status(200).json({message: 'Registration Done'});
-        }
+        res.render('admin/add-post',{
+            locals,layout: dir
+        });
     } catch(error){
-        console.error(error);
-        res.status(500).json({message : 'Internal Server Error'});
+        console.log(error);
     }
-});
+})
+
+// POST
+// ADD-POST - NEW BLOG INSERT CHECK
+
+router.post('/add-post', checkMiddleware, async (req,res)=>{
+    try {
+        const {title,body} = req.body;
+        await Post.insertMany({
+            title: title,
+            body: body
+        });
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 module.exports = router;
